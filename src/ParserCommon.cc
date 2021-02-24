eed0c7a78c17121699dbde3894c0237a3a6837a3
Benjamin Chang <benjaminchang23@gmail.com>
2021-02-23 19:28:57 -0500

    feat: fix declaration of common functions

--- src/ParserCommon.cc
+++ src/ParserCommon.cc
@@ -2,13 +2,38 @@
  * ParserCommon.cc
  */
 
-#include "ParserCommon.h"
+#include <ParserCommon.h>
 
 namespace black_library {
 
 namespace core {
 
 namespace parsers {
+
+std::string GetParserName(parser_rep rep)
+{
+    switch (rep)
+    {
+    case AO3_PARSER:
+        return "AO3_PARSER";
+        break;
+    case FFN_PARSER:
+        return "FFN_PARSER";
+        break;
+    case SBF_PARSER:
+        return "SBF_PARSER";
+        break;
+    case RR_PARSER:
+        return "RR_PARSER";
+        break;
+    case _NUM_PARSERS_TYPE:
+        return "_NUM_PARSERS_TYPE";
+        break;
+    default:
+        return "NO_PARSER";
+        break;
+    }
+}
 
 } // namespace parsers
 } // namespace core
