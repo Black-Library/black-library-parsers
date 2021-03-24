133855bc2b9b6f99531ad853f8f07dd85544c84d
Benjamin Chang <benjaminchang23@gmail.com>
2021-03-24 18:13:29 -0400

    feat: add blocking queue

---
+++ include/BlockingQueue.h
@@ -0,0 +1,42 @@
+/**
+ * ParserManager.h
+ * 
+ * Thread safe blocking queue
+ */
+
+#ifndef __BLOCKING_QUEUE_H__
+#define __BLOCKING_QUEUE_H__
+
+#include <functional>
+#include <mutex>
+#include <queue>
+
+template <typename T>
+class BlockingQueue {
+private:
+    std::mutex mutex_;
+    std::queue<T> queue_;
+public:
+    T pop() {
+        mutex_.lock();
+        T value = queue_.front();
+        queue_.pop();
+        mutex_.unlock();
+        return value;
+    }
+
+    void push(T value) {
+        mutex_.lock();
+        queue_.push(value);
+        mutex_.unlock();
+    }
+
+    bool empty() {
+        mutex_.lock();
+        bool check = queue_.empty();
+        mutex_.unlock();
+        return check;
+    }
+};
+
+#endif
