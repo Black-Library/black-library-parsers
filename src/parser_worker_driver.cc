4d22794f2cab717c4b362f902134e6e48db751d8
Benjamin Chang <benjaminchang23@gmail.com>
2021-04-03 13:42:45 -0400

    chore: fix file name typo and update name of library to be less generic

--- src/parser_worker_driver.cc
+++ src/parser_worker_driver.cc
@@ -2,9 +2,21 @@
  * parser_worker_driver.cc
  */
 
+#include <memory>
+#include <signal.h>
+
 #include <Parser.h>
+#include <ParserWorker.h>
 
 #include <ParserRR.h>
+
+black_library::core::parsers::ParserWorker *parser_worker;
+
+void SigHandler(int sig)
+{
+    if (sig == SIGTERM || sig == SIGINT)
+        parser_worker->Stop();
+}
 
 int main(int argc, char* argv[])
 {
@@ -19,11 +31,14 @@ int main(int argc, char* argv[])
         std::cout << std::endl;
     }
 
+    signal(SIGINT, SigHandler);
+    signal(SIGTERM, SigHandler);
+
     curl_global_init(CURL_GLOBAL_DEFAULT);
 
-    black_library::core::parsers::RR::ParserRR parser2;
-    parser2.SetUrl("https://www.royalroad.com/fiction/21220/mother-of-learning");
-    parser2.Parse();
+    black_library::core::parsers::ParserWorker worker(std::make_shared<black_library::core::parsers::Parser>(), 1, black_library::core::parsers::_NUM_PARSERS_TYPE);
+
+    parser_worker = &worker;
 
     curl_global_cleanup();
 
