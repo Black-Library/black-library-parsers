d32efce35bc72fe134acba9cbb3bf340c249507a
Benjamin Chang <benjaminchang23@gmail.com>
2021-04-23 17:37:29 -0400

    feat: add auto return to blocking unordered map

--- include/BlockingUnorderedMap.h
+++ include/BlockingUnorderedMap.h
@@ -18,18 +18,28 @@ private:
     std::mutex mutex_;
     std::unordered_map<Key, Value> map_;
 public:
-    // TODO: emplace actually returns something
-    void emplace(Key key, Value value)
+    decltype(auto) begin()
     {
         const std::lock_guard<std::mutex> lock(mutex_);
-        map_.emplace(key, value);
+        map_.begin();
     }
 
-    // TODO: erase actually returns something
-    void erase(Key key)
+    decltype(auto) emplace(Key key, Value value)
     {
         const std::lock_guard<std::mutex> lock(mutex_);
-        map_.erase(key);
+        return map_.emplace(key, value);
+    }
+
+    decltype(auto) erase(Key key)
+    {
+        const std::lock_guard<std::mutex> lock(mutex_);
+        return map_.erase(key);
+    }
+
+    decltype(auto) end()
+    {
+        const std::lock_guard<std::mutex> lock(mutex_);
+        map_.end();
     }
 
     bool find_and_replace(Key key, Value value)
@@ -45,13 +55,19 @@ public:
         return true;
     }
 
+    decltype(auto) find(Key key)
+    {
+        const std::lock_guard<std::mutex> lock(mutex_);
+        return map_.find(key);
+    }
+
     bool key_exists(Key key)
     {
         const std::lock_guard<std::mutex> lock(mutex_);
         if (map_.find(key) == map_.end())
             return false;
 
-        return false;
+        return true;
     }
 
     size_t size()
