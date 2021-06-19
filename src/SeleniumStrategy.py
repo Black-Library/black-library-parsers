8711fcacf193a3df074fd53e98d721ea4b2893de
Jonathan <jonchang00work@gmail.com>
2021-06-19 15:20:44 -0400

    feat Removing HTMLStrategy Stuff

--- src/SeleniumStrategy.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-def SeleniumGetPageHTML(url):
-    driver = webdriver.Firefox()
-    driver.get('http://www.ubuntu.com/')
-    print(url)
-    sleep(2)
-    page_raw = driver.page_source
-    return page_raw
